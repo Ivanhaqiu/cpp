4dc168634ee3731121a20943c406e844354269d5
IQIU <ivanqiu1026@gmail.com>
2023-11-20 22:59:24 +0800

    this 指针

--- 学习/源.cpp
+++ 学习/源.cpp
@@ -1028,3 +1028,42 @@ using namespace std;
 //	test02();
 //	return 0;
 //}
+
+//this 指针
+//class Person
+//{
+//public:
+//	Person(int age)
+//	{
+//		this->age = age;
+//	}
+//
+//	Person& PersonAddAge(Person& p)
+//	{
+//		this->age += p.age; 
+//		return *this;
+//	}
+//	int age;
+//};
+//
+//void test01()
+//{
+//	Person p(18);
+//	cout << "p的年龄为：" << p.age << endl;
+//}
+//
+//void test02()
+//{
+//	Person p1(10);
+//	Person p2(10);
+//
+//	p2.PersonAddAge(p1).PersonAddAge(p1).PersonAddAge(p1);
+//	cout << "p2 age: " << p2.age << endl;
+//}
+//
+//int main()
+//{
+//	//test01();
+//	test02();
+//	return 0;
+//}
