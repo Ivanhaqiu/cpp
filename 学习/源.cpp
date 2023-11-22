1a118c658d361eea9ded4290c456742f964d3f1c
IQIU <ivanqiu1026@gmail.com>
2023-11-22 21:58:30 +0800

    加号运算符重载

--- 学习/源.cpp
+++ 学习/源.cpp
@@ -1294,3 +1294,56 @@ using namespace std;
 //
 //	return 0;
 //}
+
+
+//¼ÓºÅÔËËã·ûÖØÔØ
+class Person
+{
+public:
+
+	int m_a;
+	int m_b;
+
+};
+
+Person operator+(Person& p1, Person& p2)
+{
+	Person tmp;
+	tmp.m_a = p1.m_a + p2.m_a;
+	tmp.m_b = p1.m_b + p2.m_b;
+	return tmp;
+}
+
+Person operator+(Person& p1, int num)
+{
+	Person tmp;
+	tmp.m_a = p1.m_a + num;
+	tmp.m_b = p1.m_b + num;
+	return tmp;
+}
+
+void test01()
+{
+	Person p1;
+	p1.m_a = 10;
+	p1.m_b = 10;
+	Person p2;
+	p2.m_a = 10;
+	p2.m_b = 10;
+
+	//Person p3 = p1.operator+(p2);
+
+	//Person p3 = operator+(p1, p2);
+	Person p3 = p1 + p2; 
+
+	Person p4 = p1 + 20;
+	cout << p3.m_a << endl << p3.m_b << endl;
+	cout << p4.m_a << endl << p4.m_b << endl;
+
+}
+
+int main()
+{
+	test01();
+	return 0;
+}
