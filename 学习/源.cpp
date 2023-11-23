9bfd5c064afca8042f8e702a585a3dbdc1d81809
IQIU <ivanqiu1026@gmail.com>
2023-11-23 22:57:30 +0800

    赋值运算符重载

--- 学习/源.cpp
+++ 学习/源.cpp
@@ -1432,3 +1432,69 @@ using namespace std;
 //	test02();
 //	return 0;
 //}
+
+
+//赋值运算符重载
+//class Person
+//{
+//public:
+//	Person(int age)
+//	{
+//		m_age = new int(age);
+//	}
+//
+//	~Person()
+//	{
+//		if (m_age != NULL)
+//		{
+//			delete m_age;
+//			m_age = NULL;
+//		}
+//	}
+//
+//	Person& operator=(Person& p)
+//	{
+//		if (m_age != NULL)
+//		{
+//			delete m_age;
+//			m_age = NULL;
+//		}
+//
+//		m_age = new int(*p.m_age);
+//		return *this;
+//	}
+//
+//	int* m_age;
+//};
+//
+//void test01()
+//{
+//	Person p1(18);
+//
+//	Person p2(20);
+//
+//	Person p3(30);
+//	p3 = p2 = p1;
+//
+//	cout << "p1的年龄为：" << *p1.m_age << endl;
+//
+//	cout << "p2的年龄为：" << *p2.m_age << endl;
+//
+//	cout << "p3的年龄为：" << *p3.m_age << endl;
+//
+//}
+//
+//int main()
+//{
+//	test01();
+//
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//
+//	c = b = a;
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//	cout << "c= " << c << endl;
+//	return 0;
+//}
