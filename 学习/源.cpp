6bf270e04328bbfa718f79be34b481bd5303591d
IQIU <ivanqiu1026@gmail.com>
2023-12-03 20:52:16 +0800

    菱形继承问题以及解决方法

--- 学习/源.cpp
+++ 学习/源.cpp
@@ -2106,3 +2106,40 @@ using namespace std;
 //	test01();
 //	return 0;
 //}
+
+
+class Animal 
+{
+public:
+	int m_age;
+};
+
+class Sheep :virtual public Animal
+{
+
+};
+
+class Tuo :virtual public Animal
+{
+
+};
+
+class SheepTuo: public Sheep, public Tuo
+{};
+
+void test01()
+{
+	SheepTuo st;
+	st.Sheep::m_age = 18;
+	st.Tuo::m_age = 28;
+
+	cout << "st.Sheep::m_age = " << st.Sheep::m_age << endl;
+	cout << "st.Tuo::m_age = " << st.Tuo::m_age << endl;
+}
+
+
+int main()
+{
+	test01();
+	return 0;
+}
