e08531b1334740034a209697218ef9f12531586a
IQIU <ivanqiu1026@gmail.com>
2024-03-26 21:28:03 +0800

    update

--- 学习/源.cpp
+++ 学习/源.cpp
@@ -3432,62 +3432,65 @@ using namespace std;
 //}
 
 //(181)模板-类模板与友元	全局函数类内实现-直接在类内申明友元即可		全局函数类外实现-需要提前让编译器知道全局函数的存在
+//
+//template<class T1,class T2>
+//class Person;
+//
+////类外实现
+//template<class T1, class T2>
+//void printPerson2(Person<T1, T2> p)
+//{
+//	cout << "类外实现 --- 姓名：" << p.m_name << "年龄：" << p.m_age << endl;
+//}
+//
+//template<class T1, class T2>
+//class Person
+//{
+//	//全局函数 类内实现
+//	friend void printPerson(Person<T1, T2> p)
+//	{
+//		cout << "姓名：" << p.m_name << " 年龄：" << p.m_age << endl;
+//	}
+//	//全局函数 类外实现
+//	//加空模板参数列表
+//	//如果是全局函数 是类外实现，需要让编译器提前知道这个函数的存在
+//	friend void printPerson2<>(Person<T1, T2> p);
+//
+//public:
+//	Person(T1 name, T2 age)
+//	{
+//		this->m_name = name;
+//		this->m_age = age;
+//	}
+//		
+//private:
+//	T1 m_name;
+//	T2 m_age;
+//};
+//
+//
+//
+////1、全局函数在类内实现
+//void test01()
+//{
+//	Person<string, int>p("tom", 20);
+//
+//	printPerson(p);
+//}
+//
+////2、全局函数在类外实现
+//void test02()
+//{
+//	Person<string, int> p("jerry", 20);
+//
+//	printPerson2(p);
+//}
+//
+//int main()
+//{
+//	test01();
+//	return 0;
+//}
 
-template<class T1,class T2>
-class Person;
 
-//类外实现
-template<class T1, class T2>
-void printPerson2(Person<T1, T2> p)
-{
-	cout << "类外实现 --- 姓名：" << p.m_name << "年龄：" << p.m_age << endl;
-}
-
-template<class T1, class T2>
-class Person
-{
-	//全局函数 类内实现
-	friend void printPerson(Person<T1, T2> p)
-	{
-		cout << "姓名：" << p.m_name << " 年龄：" << p.m_age << endl;
-	}
-	//全局函数 类外实现
-	//加空模板参数列表
-	//如果是全局函数 是类外实现，需要让编译器提前知道这个函数的存在
-	friend void printPerson2<>(Person<T1, T2> p);
-
-public:
-	Person(T1 name, T2 age)
-	{
-		this->m_name = name;
-		this->m_age = age;
-	}
-		
-private:
-	T1 m_name;
-	T2 m_age;
-};
-
-
-
-//1、全局函数在类内实现
-void test01()
-{
-	Person<string, int>p("tom", 20);
-
-	printPerson(p);
-}
-
-//2、全局函数在类外实现
-void test02()
-{
-	Person<string, int> p("jerry", 20);
-
-	printPerson2(p);
-}
-
-int main()
-{
-	test01();
-	return 0;
-}
+//(182)
